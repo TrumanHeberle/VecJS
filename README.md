a97daaeadeeec364b1a253d9833a57fefd7bae90
TrumanHeberle <trumanheberle@gmail.com>
2019-09-01 17:13:08 -0400

    added vector operations and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-VecJS
-=====
+This library provides a powerful, lightweight utility for vector operations.
+Furthermore, vectors are transferable (see [here](https://developers.google.com/web/updates/2011/12/Transferable-Objects-Lightning-Fast) for details) since the vectors utilize array buffers.
+
+## Creating Vectors
+Vectors are easily created by calling the vector constructor with an initial value for x, y, and z. Invalid values default to 0.
+
+```javascript
+var vector1 = new Vec.V3(0,1,2); // (x: 0, y: 1, z: 2)
+var vector2 = new Vec.V3(0,1); // (x: 0, y: 1, z: 0)
+var vector3 = new Vec.V3(); // (x: 0, y: 0, z: 0)
+```
+
+## Getters/Setters
+### Getting Components
+```javascript
+var vector = new Vec.V3(0,1,2); // (x: 0, y: 1, z: 2)
+console.log(vector.getX(), vector.getY(), vector.getZ()); // logs: "0 1 2"
+```
+
+## Supported Operations
+###
--- vector.js
+++ vector.js
@@ -10,22 +10,28 @@ class V3 {
   setY(y) { this.vec[1] = Number(y) === y ? Number(y) : this.vec[1]; return this; }
   setZ(z) { this.vec[2] = Number(z) === z ? Number(z) : this.vec[2]; return this; }
   setXYZ(x,y,z) { return this.setX(x).setY(y).setZ(z); }
+  set(vec) { return this.setXYZ(vec.getX(), vec.getY(), vec.getZ()); }
   addX(x) { return this.setX(this.getX() + x); }
   addY(y) { return this.setY(this.getY() + y); }
   addZ(z) { return this.setZ(this.getZ() + z); }
   addXYZ(x,y,z) { return this.addX(x).addY(y).addZ(z); }
+  add(vec) { return this.addXYZ(vec.getX(), vec.getY(), vec.getZ()); }
   subX(x) { return this.addX(-x); }
   subY(y) { return this.addY(-y); }
   subZ(z) { return this.addZ(-z); }
   subXYZ(x,y,z) { return this.subX(x).subY(y).subZ(z); }
+  sub(vec) { return this.subXYZ(vec.getX(), vec.getY(), vec.getZ()); }
   mulX(x) { return this.setX(this.getX() * x); }
   mulZ(y) { return this.setX(this.getY() * y); }
   mulY(z) { return this.setX(this.getZ() * z); }
   mulXYZ(x,y,z) { return this.mulX(x).mulY(y).mulZ(z); }
+  mul(vec) { return this.mulXYZ(vec.getX(), vec.getY(), vec.getZ()); }
   divX(x) { return this.mulX(1/x); }
   divY(y) { return this.mulY(1/y); }
   divZ(z) { return this.mulZ(1/z); }
   divXYZ(x,y,z) { return this.divX(x).divY(y).divZ(z); }
+  div(vec) { return this.divXYZ(vec.getX(), vec.getY(), vec.getZ()); }
+  scale(c) { return this.mulXYZ(c,c,c); }
 }
 
 window.Vec = { V3: V3 }
